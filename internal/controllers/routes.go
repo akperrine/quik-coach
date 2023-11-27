4b8f279e7cf2efb37faa92eabf92cd973c20e6df
Austin Perrine <akperrine@gmail.com>
2023-11-27 14:21:50 -0700

    add PUT and DELETE to allowed methods

--- internal/controllers/routes.go
+++ internal/controllers/routes.go
@@ -63,7 +63,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, *")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
 
